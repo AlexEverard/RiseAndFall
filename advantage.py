3f40a052c60eeeea83d34d28d5922f558959c95b
Michael Grazebrook <michael@grazebrook.com>
2017-10-02 20:28:09 +0100

    Nation advantages

---
+++ advantage.py
@@ -0,0 +1,15 @@
+
+advantage = (
+    (1, 'Archery'),
+    (2, 'Light Cavalry'),
+    (3, 'Heavy Cavalry'),
+    (4, 'Armoured Infantry'),
+    (5, 'Roads'),
+    (6, 'Beserk'),
+    (7, 'Death or Glory'),
+    (8, 'Hero King'),
+    (9, 'Raiders'),
+    (10, 'Farmer'),
+    (11, 'Light Infantry / Seafarers'),
+    (12, 'Engineering'),
+)
